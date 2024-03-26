2910f7c8796c5bc2154db7ae9fd32a9c1b2d4a8a
vivek3410 <87080539+vivek3410@users.noreply.github.com>
2024-03-26 11:50:20 +0530

    commit

--- app/account/edit-address/billing/page.tsx
+++ app/account/edit-address/billing/page.tsx
@@ -1,13 +1,49 @@
 'use client'
+import axios from "axios";
 import AddressForm from "../components/AddressForm";
+import { useState } from "react";
+import { Address } from "@/types";
+import toast from "react-hot-toast";
 
 interface Props {
 
 }
 
 function Page(props: Props) {
+    const [form, setForm] = useState<Address>({
+        firstName: "",
+        lastName: "",
+        city: "",
+        state: "",
+        pincode: "",
+        streetAddress: "",
+        country: "",
+        userId: "",
+    })
+    const [isSaved, setIsSaved] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const onSubmit = async (e: any) => {
+        e.preventDefault()
+        setLoading(true)
+        try {
+            // console.log(form);
+            const res = await axios.post('/api/account/shipping', form)
+            console.log(res.statusText);
+            if (res.status === 200) {
+                setIsSaved(true)
+            }
+            toast.success("saved")
+        } catch (e) {
+            toast.error("something went wrong")
+            console.log(e);
+        }
+        finally {
+            setLoading(false)
+        }
+    }
     return (
-        <AddressForm title="Billing" />
+        <AddressForm title='Shipping' form={form} setForm={setForm} onSubmit={onSubmit} isSaved={isSaved} loading={loading} />
+
     );
 }
 
