ed6c23ade1fb2005dc57c32c93e30ace41d7cc0c
vivek3410 <87080539+vivek3410@users.noreply.github.com>
2024-03-27 10:36:29 +0530

    commit

--- components/FloatingInput/index.tsx
+++ components/FloatingInput/index.tsx
@@ -40,21 +40,29 @@ export const FloatingInput = ({ label, custom, name, value, onChange, required }
                 onChange={(e) => handleInput(e)}
                 required={required}
             />
-            {/* <span className={`text-md absolute left-0 top-3 mx-4 px-2 transition duration-200 input-text ${value || inputValue ? ' bg-white transform -translate-y-6 -translate-x-2 scale-75' : 'block '}`}>{label}</span> */}
+            <span className={`text-xs absolute left-0 top-4 mx-4 px-2 transition duration-200 input-text ${value || inputValue ? ' bg-white transform -translate-y-6 -translate-x-2 scale-75 text-md' : 'block '}`}>{label}</span>
         </div>
     )
 }
 
-export const FloatingTextArea = ({ label, custom, name, value, onChange, required }: FloatingInputProps) => {
+export const FloatingTextArea = ({ label, custom, name, value, onChange, required, }: FloatingInputProps) => {
+    const [inputValue, setInputValue] = useState(value || null);
+
+    const handleInput = (e: any) => {
+        setInputValue(e.target.value)
+        if (onChange) {
+            onChange(e)
+        }
+    }
     return (
         <div className='flex justify-center items-center relative'>
             <textarea className={` px-2 md:px-4 pt-4 h-24 bg-white w-full text-xl border-2 border-black outline-none focus:border-teal-600 focus:text-black border-opacity-50 transition duration-200 ${custom}`}
                 name={name}
                 value={value}
-                onChange={onChange}
                 required={required}
+                onChange={(e) => handleInput(e)}
             />
-            <span className='text-md absolute left-0 top-3 mx-4 px-2 transition duration-200 input-text'>{label}</span>
+            <span className={`text-xs absolute left-0 top-4 mx-4 px-2 transition duration-200 input-text ${value || inputValue ? ' bg-white transform -translate-y-6 -translate-x-2 scale-75 text-md' : 'block '}`}>{label}</span>
         </div>
     )
 }
